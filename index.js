56d7e130aaeac79ae67c9368e27051ef6a568ec2
zgheibali <80042395+zgheibali@users.noreply.github.com>
2025-04-20 22:06:27 +0300

    Add logs

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
+
+  console.log(req.headers);
+  
   res.statusCode = 200;
   const msg = process.env.MSG || 'Hello AZ!\n'
   res.end(msg);
